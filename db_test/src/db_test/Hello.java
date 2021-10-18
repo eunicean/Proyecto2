905e74b32339b509ceef961aa8190caf687a65db
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2021-10-17 23:50:58 -0600

    Conexión a la base de Datos Java
    Conexión que si sirve a la base de datos desde Java.
    Se arregló:
    -Se puede enviar el query
    -Se puede obener la información

---
+++ db_test/src/db_test/Hello.java
@@ -0,0 +1,10 @@
+package db_test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		personas obj = new personas();
+		obj.get_personas();
+	}
+
+}
---
+++ db_test/src/module-info.java
@@ -0,0 +1,3 @@
+module db_test {
+	requires java.sql;
+}
